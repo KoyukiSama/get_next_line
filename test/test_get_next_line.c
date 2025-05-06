7f3779b1cde843e9f5121bca4247c8061d21e2a4
Katarina Claes <kclaes@f0r1s13.codam.nl>
2025-05-06 20:16:04 +0200

    tested 4 cases

--- test/test_get_next_line.c
+++ test/test_get_next_line.c
@@ -9,18 +9,41 @@
 
 int main(void)
 {
-	int fd = open("file.txt", O_RDONLY);
-
+	int fd1 = open("test1.txt", O_RDONLY);
+	int fd2 = open("test2.txt", O_RDONLY);
+	int fd3 = open("test3.txt", O_RDONLY);
+	int fd4 = open("test4.txt", O_RDONLY);
 	char *line;
 
-	for (int i = 0; i < 7; i++)
+	printf("0----------test1.txt | simple test----------0\n");
+	for (int i = 0; i < 4; i++)
 	{
-		line = get_next_line(0);
-		printf("\nread: %s\n", line);
+		line = get_next_line(fd1);
+		printf("%s", line);
 		free(line);
+	}
 
-		line = get_next_line(fd);
-		printf("\nread: %s\n", line);
+	printf("\n0----------test2.txt | no '\\n' at end----------0\n");
+	for (int i = 0; i < 4; i++)
+	{
+		line = get_next_line(fd2);
+		printf("%s", line);
+		free(line);
+	}
+
+	printf("\n0----------test3.txt | multiple '\\n'----------0\n");
+	for (int i = 0; i < 6; i++)
+	{
+		line = get_next_line(fd3);
+		printf("%s", line);
+		free(line);
+	}
+
+	printf("\n0----------test4.txt | one BIG line----------0\n");
+	for (int i = 0; i < 1; i++)
+	{
+		line = get_next_line(fd4);
+		printf("%s", line);
 		free(line);
 	}
 }
